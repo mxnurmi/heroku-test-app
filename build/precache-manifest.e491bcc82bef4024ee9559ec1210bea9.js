8d9da08a73bb418e9eca2286662f203a9b0c5881
Miska <miska.nurmi@helsinki.fi>
2020-06-14 18:50:18 +0300

    added frontend

---
+++ build/precache-manifest.e491bcc82bef4024ee9559ec1210bea9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4affb81d13d696788fcffb8225e3c1ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9edc8d9fa069c325c192",
+    "url": "/static/css/main.adb0e0ae.chunk.css"
+  },
+  {
+    "revision": "bf2935369bdb0fd13865",
+    "url": "/static/js/2.df962d3b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.df962d3b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9edc8d9fa069c325c192",
+    "url": "/static/js/main.8fc11af2.chunk.js"
+  },
+  {
+    "revision": "8dd7441d529e73d61d47",
+    "url": "/static/js/runtime-main.aaa24d6c.js"
+  }
+]);
